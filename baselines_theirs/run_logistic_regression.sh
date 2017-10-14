51fee6ee2a05c806656b28f6a35514116a4860d2
Anant Subramanian <anant.subramanian15@gmail.com>
2017-10-14 07:11:52 -0400

    Format LR baseline run script

--- baselines_theirs/run_logistic_regression.sh
+++ baselines_theirs/run_logistic_regression.sh
@@ -20,4 +20,18 @@ OUTPUT_PATH=output
 rm -r $OUTPUT_PATH 2>/dev/null
 mkdir $OUTPUT_PATH
 
-python -u src/learning_baseline/feature_based/train.py --input-train $DATA_PATH/train-v1.0.json --input-train-articles $PROTO_DUMPS_PATH/train-anotatedpartial/train-annotatedpartial.proto --input-train-features $PROTO_DUMPS_PATH/train-feature/train-featuresbucketized.proto --input-dev $DATA_PATH/dev-v1.0.json --input-dev-articles $PROTO_DUMPS_PATH/dev-anotatedpartial/dev-annotatedpartial.proto --input-dev-features $PROTO_DUMPS_PATH/dev-feature/dev-featuresbucketized.proto --model-output $OUTPUT_PATH/model --metrics-output $OUTPUT_PATH/metrics --dev-predictions-output $OUTPUT_PATH/dev-predictions --num-iterations $NUM_ITERS --learning-rate $LEARNING_RATE --l2 $L2_COEFF --input-featuredict $PROTO_DUMPS_PATH/train-feature/featuredictbucketized.proto --use-full-dictionary
+python -u src/learning_baseline/feature_based/train.py \
+  --input-train $DATA_PATH/train-v1.0.json \
+  --input-train-articles $PROTO_DUMPS_PATH/train-anotatedpartial/train-annotatedpartial.proto \
+  --input-train-features $PROTO_DUMPS_PATH/train-feature/train-featuresbucketized.proto \
+  --input-dev $DATA_PATH/dev-v1.0.json \
+  --input-dev-articles $PROTO_DUMPS_PATH/dev-anotatedpartial/dev-annotatedpartial.proto \
+  --input-dev-features $PROTO_DUMPS_PATH/dev-feature/dev-featuresbucketized.proto \
+  --model-output $OUTPUT_PATH/model \
+  --metrics-output $OUTPUT_PATH/metrics \
+  --dev-predictions-output $OUTPUT_PATH/dev-predictions \
+  --num-iterations $NUM_ITERS \
+  --learning-rate $LEARNING_RATE \
+  --l2 $L2_COEFF \
+  --input-featuredict $PROTO_DUMPS_PATH/train-feature/featuredictbucketized.proto \
+  --use-full-dictionary
